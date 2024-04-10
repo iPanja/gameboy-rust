44a43ec8d582fef51e268c5f4808bccc018fa7fb
iPanja <fhenneman@comcast.net>
2024-04-10 00:23:35 -0400

    reading DMG ROM

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    // Read boot ROM file
+    let mut buffer: Vec<u8> = Vec::new();
+
+    let mut file = File::open("./DMG_ROM.bin").expect("INVALID ROM");
+    file.read_to_end(&mut buffer).unwrap();
+
+    //println!("{:?}", buffer);
+    for &byte in buffer.iter() {
+        println!("{:#X}", byte);
+    }
 }
