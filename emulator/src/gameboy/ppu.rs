8f58270ad76f4416b1f202da5aa88da606bc7bae
iPanja <fhenneman@comcast.net>
2024-04-16 04:23:39 -0400

    basic display work

--- emulator/src/gameboy/ppu.rs
+++ emulator/src/gameboy/ppu.rs
@@ -1,4 +1,43 @@
-pub struct PPU {}
+use std::io::empty;
+
+use crate::{SCREEN_HEIGHT, SCREEN_WIDTH};
+
+use super::Bus;
+
+const VRAM_SIZE: usize = (0x9FFF - 0x8000) + (0xFE9F - 0xFE00);
+
+const TILE_MAP_SIZE: usize = 0x3FF;
+
+pub struct PPU {
+    vram: [u8; VRAM_SIZE],
+    tile_set: [Tile; 384],
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Mode {
+    /// In horizontal blanking
+    HBlank = 0,
+    /// In vertical blanking
+    VBlank = 1,
+    /// Accessing sprite memory, Sprite attributes RAM [0xfe00, 0xfe9f]
+    /// can't be accessed
+    Prelude = 2,
+    /// Accessing sprite memory and video memory [0x8000, 0x9fff],
+    /// both can't be accessed from CPU
+    Active = 3,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Pixel {
+    White,     // 0b11
+    DarkGray,  // 0b10
+    LightGray, // 0b01
+    Black,     // 0b00
+}
+type Tile = [[Pixel; 8]; 8];
+fn empty_tile() -> Tile {
+    [[Pixel::Black; 8]; 8]
+}
 
 /* Registers
     0xFF40 - LCDC - PPU control register
@@ -8,10 +47,13 @@ pub struct PPU {}
     0xFF44 - LY - Scanline register
 */
 
-/* VRAM Tile Data
-    Block 0 - $8000-87FF
-    Block 1 - $8800-8FFF
-    Block 2 - $9000-97FF
+/* VRAM Data
+    Tile Set Block 0 - $8000-87FF
+    Tile Set Block 1 - $8800-8FFF
+    Tile Set Block 2 - $9000-97FF
+
+    Tile Map 0 - $9800-9BFF
+    Tile Map 1 - $9C00-9FFF
 */
 
 /* Addressing
@@ -32,11 +74,18 @@ pub struct PPU {}
 
 /* Tile
     A tile assigns a color ID to a pixel, ranging from 0-3 (2 bits).
+        > 0b11 => white (W)
+        > 0b10 => dark-gray (D)
+        > 0b01 => light-gray (L)
+        > 0b00 => black (B)
     For the Background or Window, the color ID => a pallete
     For Objects, color ID 0 = transparent, 1-3 => a pallete
 
     Each tile occupies 16 bytes, where each line is represented by 2 bytes.
-    For each line:
+        > Each pixel requires 2 bits
+        > Each tile is 8x8 = 64 pixels
+        > 64 pixels * 2bits/pixel = 128 bits or 16 bytes per tile
+    Each row of a tile is 2 bytes:
         > The first byte specifies the LSB of the color ID of each pixel
         > The second byte specifies the MSB
         > In both bytes, bit 7 represents the leftmost pixel, and bit 0 the rightmost
@@ -77,6 +126,7 @@ pub struct PPU {}
 /* Object Attribute Memory (OAM) - https://gbdev.io/pandocs/OAM.html
     > Up to 40 movable objects (sprites) can be displayed on the screen
     > Only up to 10 of these can occupy the same scanline
+        > Regardless of if they are within the viewport
     > Taken from tile blocks 0 and 1, locatd at $8000-8FFF and have unsigned numbering
     > Object attributes are stored in the OAM at $FE00-FE9F
         > Byte 0 - Y Position
@@ -84,3 +134,102 @@ pub struct PPU {}
         > Byte 2 - Tile Index
         > Byte 3 - Attributes/Flags
 */
+
+impl PPU {
+    pub fn new() -> Self {
+        PPU {
+            vram: [0; VRAM_SIZE],
+            tile_set: [empty_tile(); 384],
+        }
+    }
+
+    pub fn read_byte(&self, index: usize) -> u8 {
+        self.vram[index]
+    }
+
+    pub fn write_byte(&mut self, index: usize, value: u8) {
+        //println!("offset of: {:#X}", index);
+
+        self.vram[index] = value;
+
+        // Not writing to tile set storage => no need to cache
+        if index >= 0x1800 {
+            return;
+        }
+
+        // Cache
+        let first_index = index & 0xFFFE;
+
+        let byte1 = self.vram[first_index];
+        let byte2 = self.vram[first_index + 1];
+
+        let tile_index = index / 16;
+        let row_index = (index % 16) / 2;
+        for pixel_index in 0..8 {
+            // To determine a pixel's value we must first find the corresponding bit that encodes
+            // that pixels value:
+            // 1111_1111
+            // 0123 4567
+            //
+            // As you can see the bit that corresponds to the nth pixel is the bit in the nth
+            // position *from the left*. Bits are normally indexed from the right.
+            //
+            // To find the first pixel (a.k.a pixel 0) we find the left most bit (a.k.a bit 7). For
+            // the second pixel (a.k.a pixel 1) we first the second most left bit (a.k.a bit 6) and
+            // so on.
+            //
+            // We then create a mask with a 1 at that position and 0s everywhere else.
+            //
+            // Bitwise ANDing this mask with our bytes will leave that particular bit with its
+            // original value and every other bit with a 0.
+            let mask = 1 << (7 - pixel_index);
+            let lsb = byte1 & mask;
+            let msb = byte2 & mask;
+
+            // If the masked values are not 0 the masked bit must be 1. If they are 0, the masked
+            // bit must be 0.
+            //
+            // Finally we can tell which of the four tile values the pixel is. For example, if the least
+            // significant byte's bit is 1 and the most significant byte's bit is also 1, then we
+            // have tile value `Three`.
+            let value = match (lsb != 0, msb != 0) {
+                (true, true) => Pixel::White,
+                (false, true) => Pixel::DarkGray,
+                (true, false) => Pixel::LightGray,
+                (false, false) => Pixel::Black,
+            };
+
+            self.tile_set[tile_index][row_index][pixel_index] = value;
+        }
+    }
+
+    pub fn enable_lcd(&mut self, bus: &mut Bus) {
+        bus.ram.ram[0xFF40] = 0b1010000 | self.vram[0xFF40];
+    }
+
+    pub fn get_display(
+        &self,
+        bus: &Bus,
+        scx: u8,
+        scy: u8,
+    ) -> [[Pixel; SCREEN_WIDTH]; SCREEN_HEIGHT] {
+        //self.enable_lcd();
+
+        let mut buffer = [[Pixel::White; SCREEN_WIDTH]; SCREEN_HEIGHT];
+
+        let lcdc: u8 = bus.ram_read_byte(0xFF40 as u16);
+        let addressing_mode = if lcdc & 0b10000 == 0b10000 {
+            0x8000
+        } else {
+            0x9000
+        };
+
+        let (byte1, byte2) = if addressing_mode == 0x8000 {
+            (self.tile_set[0], self.tile_set[1])
+        } else {
+            (self.tile_set[0], self.tile_set[1])
+        };
+
+        buffer
+    }
+}
--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,26 +1,128 @@
 mod gameboy;
 
-use gameboy::{GameBoy};
+use gameboy::ppu::Pixel;
+use gameboy::GameBoy;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use std::env;
 use std::fs::File;
 use std::io::Read;
 
+const SCALE: u32 = 1;
+const SCREEN_WIDTH: usize = 160;
+const SCREEN_HEIGHT: usize = 144;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const TICKS_PER_FRAME: usize = 10;
+
+fn draw_screen(emu: &mut GameBoy, canvas: &mut Canvas<Window>) {
+    // Clear canvas as black
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let screen_buf = emu.get_display();
+
+    // Set draw color to white, and update screen
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    /*
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel != Pixel::White {
+            // True => draw as white pixel
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+
+            // Draw a rectangle at (x, y) scaled up by SCALE value
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap()
+        }
+    }*/
+
+    for (y, row) in screen_buf.iter().enumerate() {
+        for (x, pixel) in row.iter().enumerate() {
+            match pixel {
+                Pixel::White => {
+                    canvas.set_draw_color(Color::RGB(255, 255, 255));
+                }
+                Pixel::LightGray => {
+                    canvas.set_draw_color(Color::RGB(255, 0, 0));
+                }
+                Pixel::DarkGray => {
+                    canvas.set_draw_color(Color::RGB(0, 255, 0));
+                }
+                Pixel::Black => {
+                    canvas.set_draw_color(Color::RGB(0, 0, 0));
+                }
+            }
+
+            let sx = ((x + y) % SCREEN_WIDTH) as u32;
+            let sy = ((x + y) % SCREEN_HEIGHT) as u32;
+
+            let rect = Rect::new((sx * SCALE) as i32, (sy * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap()
+        }
+    }
+
+    canvas.present();
+}
+
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
     // Read boot ROM file
     let mut bootstrap_buffer: Vec<u8> = Vec::new();
     let mut rom_buffer: Vec<u8> = Vec::new();
 
     let mut bootstrap_rom = File::open("../roms/DMG_ROM.bin").expect("INVALID ROM");
-    //let mut rom = File::open("../roms/individual/01-special.gb").expect("INVALID ROM");
+    let mut rom = File::open("../roms/individual/01-special.gb").expect("INVALID ROM");
     bootstrap_rom.read_to_end(&mut bootstrap_buffer).unwrap();
-    //rom.read_to_end(&mut rom_buffer).unwrap();
+    rom.read_to_end(&mut rom_buffer).unwrap();
 
     // Create emulator
     let mut gameboy = GameBoy::new();
-    gameboy.read_rom(&bootstrap_buffer);
-    //gameboy.read_rom_at(&rom_buffer, 0x101);
+    gameboy.read_rom(&rom_buffer);
+    //gameboy.read_rom(&bootstrap_buffer);
 
     // Simulate ticks
-    loop {
-        gameboy.tick();
+    // gameboy.ppu.set_enabled(&mut gameboy.bus, true);
+
+    // Setup SDL
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Game Boy Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
+
+    gameboy.enable_display();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'gameloop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'gameloop;
+                }
+                _ => (),
+            }
+        }
+
+        for _ in 0..TICKS_PER_FRAME {
+            gameboy.tick();
+        }
+        //tick_timers();
+
+        draw_screen(&mut gameboy, &mut canvas);
     }
 }
